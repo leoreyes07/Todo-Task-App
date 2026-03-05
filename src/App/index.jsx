ece4526252bf425beeb2344730ed75a66f6bdc16
Leo R <leoreyes_07@outlook.com>
2026-03-05 10:34:34 -0600

    Refactor - deleted the react context from app

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,13 +1,84 @@
 import React from 'react';
-import { AppUI } from './AppUI';
-import { TodoProvider } from '../TodoContext';
+import { useTodos } from './useTodos';
+import { TodoHeader } from '../TodoHeader';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { TodoLoading } from '../TodoLoading';
+import { TodosError } from '../TodosError';
+import { EmptyTodo } from '../EmptyTodo';
+import { CreateTodoButton } from '../CreateTodoButton';
+import { TodoForm } from '../TodoForm';
+import { Modal } from '../Modal';
 
 function App() {
+
+  const {
+    loading,
+    error,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+    completedTodos,
+    totalTodos,
+    searchValue,
+    setSearchValue,
+    addTodo,
+  } = useTodos();
+
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <React.Fragment>
+      <TodoHeader>
+        <TodoCounter
+          completedTodos={completedTodos}
+          totalTodos={totalTodos}
+        />
+        <TodoSearch
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
+      </TodoHeader>
+
+
+
+      <TodoList>
+        {loading && (
+          <>
+            <TodoLoading />
+          </>
+        )}
+        {error && <TodosError />}
+        {(!loading && searchedTodos.length === 0) && <EmptyTodo />}
+
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton
+        setOpenModal={setOpenModal}
+      />
+
+      {openModal && (
+        <Modal>
+          <TodoForm 
+            addTodo={addTodo}
+            setOpenModal={setOpenModal}
+          />
+        </Modal>
+      )}
+    </React.Fragment>
   );
+
 }
 
 export default App;
